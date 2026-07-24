98a663ec185b494c15bc95049338246fdc6fef9c
CaveDwellerTiger <jsd1536@g.rit.edu>
2026-07-24 01:18:33 -0400

    Prepped the state machine

---
+++ Scenes/CombatMain.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class CombatMain : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnAttackButtonPressed()
+	{
+		
+	}
+
+	private void OnAttack1Pressed()
+	{
+		
+	}
+
+	private void OnYesButtonPressed()
+	{
+		
+	}
+
+	private void OnNoButtonPressed()
+	{
+		
+	}
+}
