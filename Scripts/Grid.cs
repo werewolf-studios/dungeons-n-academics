50b3834200bd9097332ea22f744793847cd15265
Minemaster429 <samtpennington2006@gmail.com>
2026-08-11 19:56:44 -0400

    E

--- Scripts/Grid.cs
+++ Scripts/Grid.cs
@@ -3,14 +3,23 @@ using System;
 
 public partial class Grid : GridMap
 {
+
     public override void _Ready()
     {
-        foreach (Node child in GetChildren())
+        foreach (Node3D child in GetChildren())
         {
             if (child is PushBlock box)
             {
                 box.Initialize(this);
             }
+            else
+            {
+                // *Nonfunctional*
+                ////Sets any other objects in gridmap to align with it(such as puzzle walls)
+                //Vector3I map = new Vector3I(0, -1, 0);
+                //var localPos = this.MapToLocal(Vector3I.Zero);
+                //child.GlobalPosition = this.ToGlobal(localPos);
+             }
         }
         
     }
