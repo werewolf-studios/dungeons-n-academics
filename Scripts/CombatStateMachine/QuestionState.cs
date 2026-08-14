28fadb7cd5793081f37f1686befbac0643dee82b
CaveDwellerTiger <jsd1536@g.rit.edu>
2026-08-14 16:18:47 -0400

    attempting to implement the question system

--- Scripts/CombatStateMachine/QuestionState.cs
+++ Scripts/CombatStateMachine/QuestionState.cs
@@ -3,26 +3,66 @@ using System;
 
 public partial class QuestionState : CombatState
 {
-	[Export]
-	public Control QuestionUI { get; set; }
+	//[Export]
+	//public NodePath PlayerStatsUI { get; set; }
+
+	//[Export]
+	//public NodePath QuestionManager { get; set; }
+
+	private CanvasLayer playerStatsUI;
+	private QuestionManager questionManager;
+
+	// The enumeration is in the QuestionManager script
+	private Difficulty currentDifficulty = Difficulty.Easy;
+
+	private bool answerCorrect = false;
+
+    public override void _Ready()
+    {
+        playerStatsUI = GetTree().Root.FindChild("PlayerStatsUI", true, false) as CanvasLayer;
+		questionManager = GetTree().Root.FindChild("QuestionsUI", true, false) as QuestionManager;
+
+		if (playerStatsUI == null) GD.Print("PlayerStatsUI completely missing");
+		if (questionManager == null) GD.Print("QuestionManager completely missing");
+    }
+
     public override void Enter()
     {
 		GD.Print("Entered QuestionState");
-        QuestionUI.Show();
+
+		if (playerStatsUI == null || questionManager == null)
+		{
+			GD.Print("Question system nodes are null");
+		}
+
+		playerStatsUI.Visible = true;
+		questionManager.StartQuestionSequence(Grade.Eighth, QuestionType.Math, Topic.Geometry, currentDifficulty, 1, 20);
     }
 
 	public override void Exit()
 	{
-		QuestionUI.Hide();
+		playerStatsUI.Visible = false;
 	}
 
-	private void OnYesButtonPressed()
+	private void OnQuestionsUIQuestionSequenceEnded()
 	{
-		csm.TransitionTo("PlayerAttackState");
+		if (answerCorrect)
+		{
+			csm.TransitionTo("PlayerAttackState");
+		}
+		else
+		{
+			csm.TransitionTo("EnemyAttackState");
+		}
 	}
 
-	private void OnNoButtonPressed()
+	private void OnQuestionsUICorrectAnswer()
 	{
-		csm.TransitionTo("EnemyAttackState");
+		answerCorrect = true;
+	}
+
+	private void OnQuestionsUIWrongAnswer()
+	{
+		answerCorrect = false;
 	}
 }
